89af5ffa666946525ca085ba5786f02d7c4e8285
Astrro MC <you@example.com>
2026-07-26 01:27:48 +0530

    [+] Updated Features

--- src/commands/ticket/ticket.js
+++ src/commands/ticket/ticket.js
@@ -2,8 +2,9 @@ import { SlashCommandBuilder, PermissionFlagsBits, ActionRowBuilder, ButtonBuild
 import { checkPermissions, requiredPerms } from '../../utils/permissions.js';
 import { successEmbed, errorEmbed, simpleEmbed, getLogoUrl } from '../../utils/embed.js';
 import { TicketSettingsModel } from '../../models/TicketSettings.js';
+import { config } from '../../config.js';
 
-export const setupState = new Map();
+const setupState = new Map();
 
 export const data = new SlashCommandBuilder()
   .setName('ticket')
